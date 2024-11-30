955ed720a1925148a4c6da765bfd561f2e24183b
AlexBond2009 <alexeybondarenko3010@gmail.com>
2024-11-30 13:15:14 +0500

    Create amogus.py

---
+++ amogus.py
@@ -0,0 +1,12 @@
+amogus = True
+while amogus:
+    if input() == "sys":
+        amogus = False
+print("      #######   ")
+print("    ###########")
+print(" #","#"*14,sep="")
+print(" #","#"*14,sep="")
+print(" #","#"*14,sep="")
+print("   ","#"*13,sep="")
+print("   ####     ####")
+print("   ####     ####")
